f11793ba8e268553a896bbec41504a61531d9fd7
SebastiaanvanVliet <690592@student.inholland.nl>
2022-09-19 11:27:40 +0200

    done

--- GradenGroupUI/MainForm.Designer.cs
+++ GradenGroupUI/MainForm.Designer.cs
@@ -28,65 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.newUser = new System.Windows.Forms.Label();
-            this.firstNameTextBox = new System.Windows.Forms.TextBox();
-            this.lastNameTextBox = new System.Windows.Forms.TextBox();
-            this.addUserButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // newUser
-            // 
-            this.newUser.AutoSize = true;
-            this.newUser.Location = new System.Drawing.Point(61, 198);
-            this.newUser.Name = "newUser";
-            this.newUser.Size = new System.Drawing.Size(50, 20);
-            this.newUser.TabIndex = 0;
-            this.newUser.Text = "label1";
-            // 
-            // firstNameTextBox
-            // 
-            this.firstNameTextBox.Location = new System.Drawing.Point(61, 59);
-            this.firstNameTextBox.Name = "firstNameTextBox";
-            this.firstNameTextBox.Size = new System.Drawing.Size(125, 27);
-            this.firstNameTextBox.TabIndex = 1;
-            // 
-            // lastNameTextBox
-            // 
-            this.lastNameTextBox.Location = new System.Drawing.Point(61, 113);
-            this.lastNameTextBox.Name = "lastNameTextBox";
-            this.lastNameTextBox.Size = new System.Drawing.Size(125, 27);
-            this.lastNameTextBox.TabIndex = 2;
-            // 
-            // addUserButton
-            // 
-            this.addUserButton.Location = new System.Drawing.Point(61, 157);
-            this.addUserButton.Name = "addUserButton";
-            this.addUserButton.Size = new System.Drawing.Size(94, 29);
-            this.addUserButton.TabIndex = 3;
-            this.addUserButton.Text = "add user";
-            this.addUserButton.UseVisualStyleBackColor = true;
-            // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.addUserButton);
-            this.Controls.Add(this.lastNameTextBox);
-            this.Controls.Add(this.firstNameTextBox);
-            this.Controls.Add(this.newUser);
-            this.Name = "Form1";
+            this.Name = "MainForm";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label newUser;
-        private System.Windows.Forms.TextBox firstNameTextBox;
-        private System.Windows.Forms.TextBox lastNameTextBox;
-        private System.Windows.Forms.Button addUserButton;
     }
 }
