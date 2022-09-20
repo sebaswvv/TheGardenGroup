19f773fd584592e676009efa8b16062df1a607c7
Tiemen <Tiemen2@live.nl>
2022-09-20 12:51:12 +0200

    api opzet

---
+++ api/src/index.ts
@@ -0,0 +1,23 @@
+import { attachControllers } from "@decorators/express"
+import { config } from "dotenv"
+import express from "express"
+import { connect } from "mongoose"
+import { EmployeesController } from "./controllers/EmployeesController"
+import { TicketsController } from "./controllers/TicketsController"
+
+config()
+
+const PORT = 2002
+const app = express()
+
+run().catch(err => console.error("Error while trying to connect to database:", err))
+
+async function run() {
+    await connect(process.env.MONGODB_STRING, { dbName: "Main" })
+}
+
+attachControllers(app, [EmployeesController, TicketsController])
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}\nhttp://localhost:${PORT}`)
+})
