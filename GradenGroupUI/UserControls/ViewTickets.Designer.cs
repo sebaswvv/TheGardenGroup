18ebf8272904be6e002ac53c3d341a6e6100f534
SebastiaanvanVliet <690592@student.inholland.nl>
2022-09-21 19:09:44 +0200

    .......

--- GradenGroupUI/UserControls/ViewTickets.Designer.cs
+++ GradenGroupUI/UserControls/ViewTickets.Designer.cs
@@ -1,13 +1,13 @@
-﻿namespace GradenGroupUI
+﻿namespace GradenGroupUI.UserControls
 {
-    partial class RegularEmployeeForm
+    partial class ViewTickets
     {
-        /// <summary>
+        /// <summary> 
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,25 +20,16 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Component Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // RegularEmployeeForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(735, 371);
-            this.Name = "RegularEmployeeForm";
-            this.Text = "RegularEmployeeForm";
-            this.ResumeLayout(false);
-
         }
 
         #endregion
