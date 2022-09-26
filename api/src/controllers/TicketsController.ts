6ca37656df371e375655a50366296b26d6fc9005
SebastiaanvanVliet <690592@student.inholland.nl>
2022-09-26 13:16:11 +0200

    Update TicketsController.ts

--- api/src/controllers/TicketsController.ts
+++ api/src/controllers/TicketsController.ts
@@ -1,11 +1,11 @@
-import { Body, Controller, Get, Params, Post, Request, Response } from "@decorators/express"
+import { Controller, Get, Params, Patch, Post, Request, Response, Delete } from "@decorators/express"
 import type { Response as RESTResponse } from "express"
-import { request } from "http"
 import { Ticket } from "src/models/Ticket"
 
 @Controller("/tickets")
 export class TicketsController {
-  @Get("/:id")
+    // get ticket by ticket id
+    @Get("/:id")
     async getEmployee(@Response() res: RESTResponse, @Params("id") id: string) {
         try {
             const ticket = await Ticket.findById(id)            
@@ -14,21 +14,67 @@ export class TicketsController {
             console.error(err)
             res.status(500).send("error")
         }
+    }   
+
+    // get all tickets
+    @Get("/")
+    async getEmployees(@Response() res: RESTResponse) {
+        try {
+            const tickets = await Ticket.find()
+            res.status(200).json({
+                message: "success",
+                amout: tickets.length,
+                tickets,
+            })
+        } catch (err) {
+            console.error(err)
+            res.status(500).send("error")
+        }
     }
 
-   @Post("/")
-  async updateEmployee(@Response() res: RESTResponse, @Request() request: any) {
-      try {        
-          const ticket = new Ticket(request.body)         
-          ticket.save()     
-          res.status(200).json({
-              message: "success",
-              ticket,               
-          })
-      } catch (err) {
-          console.error(err)
-          res.status(500).send("error")
-      }
-  } 
+    // create a new ticket
+    @Post("/")
+    async addEmployee(@Response() res: RESTResponse, @Request() request: any) {
+        try {        
+            const ticket = new Ticket(request.body)         
+            ticket.save()     
+            res.status(200).json({
+                message: "success",
+                ticket,               
+            })
+        } catch (err) {
+            console.error(err)
+            res.status(500).send("error")
+        }
+    } 
+
+    // update a ticket
+    @Patch("/:id")
+    async updateEmployee(@Response() res: RESTResponse, @Params("id") id: string, @Request() request: any) {
+        try {
+            const ticket = await Ticket.findByIdAndUpdate(id, request.body, { new: true })                
+            res.status(200).json({
+                message: "success",
+                ticket,
+            })
+        } catch (err) {
+            console.error(err)
+            res.status(500).send("error")
+        }
+    }
+
+    // delete a ticket
+    @Delete("/:id")
+    async deleteEmployee(@Response() res: RESTResponse, @Params("id") id: string) {
+        try {
+            await Ticket.findByIdAndDelete(id)
+            res.status(200).json({
+                message: "success",
+            })
+        } catch (err) {
+            console.error(err)
+            res.status(500).send("error")
+        }
+    }
 }
 
