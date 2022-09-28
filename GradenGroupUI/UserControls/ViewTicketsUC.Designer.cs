feebfc07b2210ee1e50ff10136110bd89dfdc024
SebastiaanvanVliet <690592@student.inholland.nl>
2022-09-28 13:09:12 +0200

    ...

--- GradenGroupUI/UserControls/ViewTicketsUC.Designer.cs
+++ GradenGroupUI/UserControls/ViewTicketsUC.Designer.cs
@@ -33,6 +33,7 @@
             this.showDashboardTicketsButton = new System.Windows.Forms.Button();
             this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.pieChartPanel = new System.Windows.Forms.Panel();
+            this.panelViewTickets = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,9 +41,10 @@
             // 
             this.showTicketsButton.BackColor = System.Drawing.SystemColors.HotTrack;
             this.showTicketsButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.showTicketsButton.Location = new System.Drawing.Point(39, 27);
+            this.showTicketsButton.Location = new System.Drawing.Point(45, 36);
+            this.showTicketsButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.showTicketsButton.Name = "showTicketsButton";
-            this.showTicketsButton.Size = new System.Drawing.Size(166, 44);
+            this.showTicketsButton.Size = new System.Drawing.Size(190, 59);
             this.showTicketsButton.TabIndex = 0;
             this.showTicketsButton.Text = "Show all my tickets";
             this.showTicketsButton.UseVisualStyleBackColor = false;
@@ -52,9 +54,10 @@
             // 
             this.showDashboardTicketsButton.BackColor = System.Drawing.SystemColors.HotTrack;
             this.showDashboardTicketsButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.showDashboardTicketsButton.Location = new System.Drawing.Point(39, 88);
+            this.showDashboardTicketsButton.Location = new System.Drawing.Point(45, 117);
+            this.showDashboardTicketsButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.showDashboardTicketsButton.Name = "showDashboardTicketsButton";
-            this.showDashboardTicketsButton.Size = new System.Drawing.Size(166, 44);
+            this.showDashboardTicketsButton.Size = new System.Drawing.Size(190, 59);
             this.showDashboardTicketsButton.TabIndex = 1;
             this.showDashboardTicketsButton.Text = "Show dashboard";
             this.showDashboardTicketsButton.UseVisualStyleBackColor = false;
@@ -62,20 +65,30 @@
             // 
             // pieChartPanel
             // 
-            this.pieChartPanel.Location = new System.Drawing.Point(345, 139);
+            this.pieChartPanel.Location = new System.Drawing.Point(394, 185);
+            this.pieChartPanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pieChartPanel.Name = "pieChartPanel";
-            this.pieChartPanel.Size = new System.Drawing.Size(564, 397);
+            this.pieChartPanel.Size = new System.Drawing.Size(645, 529);
             this.pieChartPanel.TabIndex = 2;
+            // 
+            // panelViewTickets
+            // 
+            this.panelViewTickets.Location = new System.Drawing.Point(345, 87);
+            this.panelViewTickets.Name = "panelViewTickets";
+            this.panelViewTickets.Size = new System.Drawing.Size(804, 641);
+            this.panelViewTickets.TabIndex = 3;
             // 
             // ViewTicketsUC
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panelViewTickets);
             this.Controls.Add(this.pieChartPanel);
             this.Controls.Add(this.showDashboardTicketsButton);
             this.Controls.Add(this.showTicketsButton);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "ViewTicketsUC";
-            this.Size = new System.Drawing.Size(1412, 728);
+            this.Size = new System.Drawing.Size(1614, 971);
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
 
@@ -87,5 +100,6 @@
         private System.Windows.Forms.Button showDashboardTicketsButton;
         private System.Windows.Forms.BindingSource bindingSource1;
         private System.Windows.Forms.Panel pieChartPanel;
+        private System.Windows.Forms.Panel panelViewTickets;
     }
 }
