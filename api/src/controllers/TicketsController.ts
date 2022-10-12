d907d66c3a05185fe53465f67a3f7b29c5b4ad05
SebastiaanvanVliet <690592@student.inholland.nl>
2022-10-12 14:18:18 +0200

    Update TicketsController.ts

--- api/src/controllers/TicketsController.ts
+++ api/src/controllers/TicketsController.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Params, Patch, Post, Response, Delete, Request, Query } from "@decorators/express"
-import type { query, Request as RESTRequest, Response as RESTResponse } from "express"
-import { Status, Ticket } from "src/models/Ticket"
+import type { Response as RESTResponse } from "express"
+import { Ticket } from "src/models/Ticket"
 
 @Controller("/tickets")
 export class TicketsController {    
@@ -33,18 +33,24 @@ export class TicketsController {
             if (query.Deadline) newQuery["Deadline"] = query.Deadline                                
 
             let tickets
-            query ? tickets = await Ticket.find(newQuery) : tickets = await Ticket.find()            
-            res.status(200).json({
-                message: "success",
-                amount: tickets.length,
-                tickets,
-            })
-            res.send("hello")
-                                         
+            // check if there are any query params and find tickets based on them
+            query ? tickets = await Ticket.find(newQuery) : tickets = await Ticket.find() 
+
+            // if there are no tickets, return an error
+            tickets.length > 0 
+                ? res.status(200).json({
+                    message: "success",
+                    amount: tickets.length,
+                    tickets,
+                }) 
+                : res.status(404).json({
+                    message: "error",
+                    description: "Sorry, we couldn't find any tickets with those parameters",
+                })                                                 
         } catch (err) {            
-            res.status(500).send({
+            res.status(400).send({
                 message: "error",
-                description: "We couldn't find any tickets with those parameters, please try again",
+                description: "There was an error getting the tickets, please try again",
             })            
         }
     }
@@ -53,12 +59,22 @@ export class TicketsController {
     @Post("/")
     async createTicket(@Response() res: RESTResponse, @Request() request: any) {
         try {
+            // make a new ticket based on the request body            
             const ticket = new Ticket(request.body)
-            ticket.save()
-            res.status(200).json({
-                message: "success",
-                ticket,
-            })
+
+            // check if all the field for the ticket are present
+            if (!ticket.EmployeeID || !ticket.DateReported || !ticket.Subject || !ticket.Priority || !ticket.Deadline || !ticket.Description || !ticket.Status) {
+                res.status(400).json({
+                    message: "error",
+                    description: "Please make sure all the fields are filled out",
+                })
+            } else {
+                ticket.save()
+                res.status(200).json({
+                    message: "success",
+                    ticket,
+                }) 
+            }            
         } catch (err) {
             res.status(400).send({
                 message: "error",
@@ -71,15 +87,15 @@ export class TicketsController {
     @Patch("/:id")
     async updateTicket(@Response() res: RESTResponse, @Params("id") id: string, @Request() request: any) {
         try {
-            const ticket = await Ticket.findByIdAndUpdate(id, request.body, { new: true })
+            const ticket = await Ticket.findByIdAndUpdate(id, request.body, { new: true })                    
             res.status(200).json({
-                message: "success",
+                message: "successfully updated ticket",
                 ticket,
             })
         } catch (err) {
-            res.status(400).send({
+            res.status(404).send({
                 message: "error",
-                description: "There was an error updating the ticket, please try again",
+                description: "We couldn't find a ticket with that ID, please try again",
             })  
         }
     }
@@ -91,7 +107,7 @@ export class TicketsController {
             await Ticket.findByIdAndDelete(id)
             res.status(200).json({
                 message: "successfully deleted ticket",
-            })
+            })            
         } catch (err) {
             res.status(400).send({
                 message: "error",
